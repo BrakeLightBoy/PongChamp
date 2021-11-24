67d8e3fefbf02890c220ebeb7756010a84fa5a25
NoahJ <gusnoahjo@student.gu.se>
2021-11-24 12:53:27 +0100

    outline for project with basic functions added

--- src/main/java/pongchamp/pongchamp/HelloApplication.java
+++ src/main/java/pongchamp/pongchamp/HelloApplication.java
@@ -1,23 +1,36 @@
-package pongchamp.pongchamp;
+package com.example.pongchamp;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
+    public HelloApplication() {
+    }
+
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(getClass().getResource("Starter.fxml"));
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        /*
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene((Parent)fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        String css = this.getClass().getResource("application.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.show();
+         */
+
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(new String[0]);
     }
 }
--- src/main/java/pongchamp/pongchamp/HelloController.java
+++ src/main/java/pongchamp/pongchamp/HelloController.java
@@ -1,14 +1,48 @@
-package pongchamp.pongchamp;
+package com.example.pongchamp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button logoutButton;
+    @FXML
+    private AnchorPane mainPane;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToSettings(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Settings.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+    public void switchToMain(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void logout(ActionEvent event) {
+        stage = (Stage) mainPane.getScene().getWindow();
+        stage.close();
+    }
+
 }
