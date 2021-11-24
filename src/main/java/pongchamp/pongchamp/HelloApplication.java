52153718a66cf6f8312173781e43d87b0109e62b
Felix Valkama <gusvalkfe@student.gu.se>
2021-11-24 21:41:31 +0100

    Reworked the start and implemented functionality to resize the window freely

--- src/main/java/pongchamp/pongchamp/HelloApplication.java
+++ src/main/java/pongchamp/pongchamp/HelloApplication.java
@@ -2,9 +2,15 @@ package pongchamp.pongchamp;
 
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.SVGPath;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -13,24 +19,24 @@ public class HelloApplication extends Application {
     }
 
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Starter.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        try{
+            BorderPane root = new BorderPane();
+            ImageView imageView = new ImageView();
+            imageView.setImage(new Image("file:D:\\Programming\\Java\\PongChamp\\pongchamp\\src\\main\\resources\\pongchamp\\pongchamp\\Start.PNG"));
+            imageView.fitWidthProperty().bind(stage.widthProperty());
+            imageView.fitHeightProperty().bind(stage.heightProperty());
+            imageView.setPreserveRatio(true);
+            root.setCenter(imageView);
+            Scene scene = new Scene(root);
 
-        /*
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene((Parent)fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        String css = this.getClass().getResource("application.css").toExternalForm();
-        scene.getStylesheets().add(css);
-        stage.show();
-         */
-
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        launch(new String[0]);
+        launch();
     }
 }
