2bfa1caed8109f22b8273756bd527bc9d5aaae09
wojpec <guspecwo@student.gu.se>
2021-12-30 14:45:56 +0100

    Started cleaning backend code and started implementing the JSON reading and writing. - Coded with Peyman and Michael.

---
+++ src/main/java/pongchamp/pongchamp/HelloApplication.java
@@ -0,0 +1,57 @@
+package pongchamp.pongchamp;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import pongchamp.pongchamp.model.Properties;
+
+import static pongchamp.pongchamp.util.FrontendMethods.*;
+import pongchamp.pongchamp.model.Properties;
+
+import java.io.IOException;
+
+
+    public class HelloApplication extends Application {
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+        @Override
+        public void start(Stage stage) throws Exception {
+            stage.setTitle("PONG");
+
+            double[] gamemode1 = {(double) Properties.BOARD_WIDTH*0.40,(double) Properties.BOARD_HEIGHT*0.52};
+            double[] gamemode2 = {(double) Properties.BOARD_WIDTH*0.50,(double) Properties.BOARD_HEIGHT*0.52};
+            double[] gamemode3 = {(double) Properties.BOARD_WIDTH*0.55,(double) Properties.BOARD_HEIGHT*0.52};
+
+            Button gamemode1Btn = createButton("btn1", "Gamemode 1", true, gamemode1,  e-> {
+
+            });
+            Button gamemode2Btn = createButton("btn2", "Gamemode 2", true, gamemode2, e-> {
+
+            });
+            Button gamemode3Btn = createButton("btn3", "Gamemode 3", true, gamemode3, e-> {
+
+            });
+
+            StackPane stackPane = new StackPane();
+            stackPane.getChildren().addAll(gamemode1Btn, gamemode2Btn, gamemode3Btn);
+
+            Scene scene = new Scene(stackPane, Properties.BOARD_WIDTH, Properties.BOARD_HEIGHT);
+            stage.setScene(scene);
+            stage.show();
+
+
+
+        }
+
+    }
+
+
+
---
+++ src/main/java/pongchamp/pongchamp/model/BoardState.java
@@ -0,0 +1,16 @@
+package pongchamp.pongchamp.model;
+
+import java.awt.*;
+
+public class BoardState {
+
+
+
+    class PaddleData{
+        private Color color;
+        private Point location;
+    }
+    class BallData{
+
+    }
+}
